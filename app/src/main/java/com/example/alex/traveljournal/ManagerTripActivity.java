3b8f2fef6bfa03b7f2d9567f99a11a2479871d33
smaidd <ionita.alex16@gmail.com>
2018-11-25 19:09:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alex/traveljournal/ManagerTripActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex.traveljournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_trip);
+    }
+}
