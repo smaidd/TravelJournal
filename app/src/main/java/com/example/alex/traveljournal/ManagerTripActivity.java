d1b774bb8fb48e14f481b5c493c8990def0576d6
alexutu100 <mgm.cartofior@yahoo.ro>
2019-01-10 22:24:45 +0200

    add_seekbar_update

--- app/src/main/java/com/example/alex/traveljournal/ManagerTripActivity.java
+++ app/src/main/java/com/example/alex/traveljournal/ManagerTripActivity.java
@@ -2,12 +2,43 @@ package com.example.alex.traveljournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ManagerTripActivity extends AppCompatActivity {
+    private SeekBar mSeekBar;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_trip);
+        initView();
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mTextView.setText("Price(euro) " + "( " + progress + " €)");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+    private void initView() {
+        mSeekBar = findViewById(R.id.seek_bar);
+        mTextView = findViewById(R.id.text_view_price);
+    }
+
+
 }
